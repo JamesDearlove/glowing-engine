bbe07cad40832cb750c21adface6fcf7d19ad08a
James Dearlove <jamesdearlove@outlook.com>
2022-08-28 13:52:18 +1000

    Chaos

--- backend/main.py
+++ backend/main.py
@@ -11,28 +11,33 @@ app = FastAPI()
 def generate_hash_signature(secret: bytes, payload: bytes, digest_method=hashlib.sha1):
     return hmac.new(secret, payload, digest_method).hexdigest()
 
-def handle_webhooks(request: Request, event: str):
+async def handle_webhooks(request: Request, event: str):
+
+    body = await request.json()
     address = os.environ.get("PI_ADDRESS")
 
     if event == "ping":
         r = httpx.post(f"{address}/chaos")
-        print(r)
-    elif event == "pull_request":
+    elif event.startswith("pull_request"):
         r = httpx.post(f"{address}/pulse", 
             json={"colors": [0x00FF50], "reverse": False})
-        print(r)
-    elif event == "issue":
-        r = httpx.post(f"{address}/pulse", 
-            json={"colors": [0x0ebdf2], "reverse": False})
-        print(r)
+    elif event.startswith("issues"):
+
+        if body["action"] == "closed":
+            r = httpx.post(f"{address}/pulse", 
+                json={"colors": [0x72ff72, 0x00FF00, 0x72ff72], "reverse": False})
+        elif body["action"] == "opened":
+            r = httpx.post(f"{address}/pulse", 
+                json={"colors": [0x0050c9, 0x0ebdf2, 0x0050c9], "reverse": False})
+
+
     elif event == "push":
         r = httpx.post(f"{address}/pulse", 
-            json={"colors": [0xFF0000], "reverse": False})
-        print(r)
+            json={"colors": [0xff7572, 0xFF0000, 0xff7572], "reverse": False})
 
 @app.get("/")
 async def root():
-    return { "Hello", "World!"}
+    return { "Hey", "World!"}
 
 @app.post("/webhook", status_code=http.HTTPStatus.ACCEPTED)
 async def webhook(request: Request, x_hub_signature: str = Header(None), x_github_event: str = Header(None)):
@@ -40,10 +45,11 @@ async def webhook(request: Request, x_hub_signature: str = Header(None), x_githu
     secret = os.environ.get("WEBHOOK_SECRET").encode("UTF-8")
     signature = generate_hash_signature(secret, payload)
 
+
     if x_hub_signature != f"sha1={signature}":
         raise HTTPException(status_code=401, detail="Authentication error.")
 
     if x_github_event != "":
-        handle_webhooks(request, x_github_event)
+        await handle_webhooks(request, x_github_event)
 
     return {}
